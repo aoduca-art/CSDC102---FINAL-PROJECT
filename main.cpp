205f5dbcf46755b7db909f33a6ff2db224170882
aoduca-art <aoduca@gbox.adnu.edu.ph>
2026-05-05 18:52:57 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+
+using namespace std;
+
+void titleScreen(){
+    
+ 
+ cout << " ____                 _	  ____                  _\n";
+ cout << "|  _ \ ___   __ _  __| |  / ___|_ __ ___  ___ ___(_)_ __   __ _\n";
+ cout << "| |_) / _ \ / _` |/ _` | | |   | '__/ _ \/ __/ __| | '_ \ / _` |\n";
+ cout << "|  _ < (_) | (_| | (_| | | |___| | | (_) \__ \__ \ | | | | (_| |\n";
+ cout << "|_| \_\___/ \__,_|\__,_|  \____|_|  \___/|___/___/_|_| |_|\__, |\n";
+ cout << "          	C h a l l e n g e                       	|___/\n\n";
+ 
+ cout << " Written for CSDC102 | Language: C++\n\n";
+ 
+ cout << " HOW TO PLAY:\n";
+ cout << " - Move with Arrow Keys\n";
+ cout << " - Dodge trucks (#####) in the ROAD ZONE\n";
+ cout << " - Hop on logs (====) in the RIVER ZONE\n";
+ cout << " - Reach the finish line 5 times to win!\n\n";
+ 
+ cout << " Press ENTER to start...\n";
+}
+
+
+
+
+
+
+int main(){
+    
+    titleScreen();
+    
+    
+return 0;
+}
